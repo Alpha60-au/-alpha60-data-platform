b4f19441eb092e029381b5a767c6e538504ed0e5
Alex Cleary <YOUR_EMAIL_ADDRESSalex@alpha60.com.au>
2026-07-08 14:10:26 +1000

    Add rotation eligibility to locations

--- dbt/alpha60/models/marts/dim_locations.sql
+++ dbt/alpha60/models/marts/dim_locations.sql
@@ -40,6 +40,41 @@ SELECT
         ELSE 'inactive'
     END AS location_type,
 
+    CASE
+        WHEN shopify_locations.inventory_location_name IN (
+            'Online / Warehouse',
+            'Faulty Land',
+            'Sparehouse',
+            'Melbourne Outlet',
+            'Spring1883'
+        ) THEN FALSE
+        ELSE TRUE
+    END AS can_send_rotations,
+
+    CASE
+        WHEN shopify_locations.inventory_location_name IN (
+            'Claremont',
+            'Wellington',
+            'James St',
+            'Online / Warehouse',
+            'Faulty Land',
+            'Sparehouse',
+            'Melbourne Outlet',
+            'Spring1883'
+        ) THEN FALSE
+        ELSE TRUE
+    END AS can_receive_rotations,
+
+    CASE
+        WHEN shopify_locations.inventory_location_name = 'Flinders Lane' THEN 100
+        WHEN shopify_locations.inventory_location_name = 'Fitzroy' THEN 95
+        WHEN shopify_locations.inventory_location_name = 'Smith St' THEN 90
+        WHEN shopify_locations.inventory_location_name = 'Newtown' THEN 85
+        WHEN shopify_locations.inventory_location_name = 'Oxford St' THEN 80
+        WHEN shopify_locations.inventory_location_name = 'Paddington' THEN 70
+        ELSE 0
+    END AS rotation_priority,
+
     CURRENT_TIMESTAMP() AS modelled_at
 
 FROM shopify_locations
